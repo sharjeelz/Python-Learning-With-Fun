21ea151afd5b1938058fff77608ac58bf7ea59df
Sharjeel Zubair <szubair01@gmail.com>
2021-08-22 13:46:40 +0500

    Update coffee_maker.py

--- coffee_maker.py
+++ coffee_maker.py
@@ -1,7 +1,7 @@
 print("KARAK CHAI MAKER")
 is_machine_on = True
 profit = 0
-
+CURRENCY = "DHS"
 MENU = {
     "karak": {
         "ingredients": {
@@ -27,39 +27,50 @@ resources = {
 }
 
 
-def check_resources(order_ingredients):
+def check_resources(order_ingredients, qty):
     for item in order_ingredients:
-        if order_ingredients[item] > resources[item]:
-            print(f"Not Enough {item}")
+        if order_ingredients[item] * qty > resources[item]:
+            print(f"Not Enough {item}, Please Ask on the counter to refill")
             return False
     return True
 
 
 def process_coins():
     print("Insert coins")
-    return float(input("Enter Dirhams: "))
+    return float(input(f"Enter Dirhams: {CURRENCY}  "))
 
 
-def is_payment_done(money, cost):
+def is_payment_done(money, cost, qty):
     global profit
     if money >= cost:
-        change = money - cost
-        print(f"here is your change {change}")
+        change = money - cost * qty
+        print(f"here is your change {change} {CURRENCY} ")
         profit += cost
         return True
     else:
-        print("Sorry that's not enough money. Money refunded.")
+        print(f"Sorry that's not enough money.{money} {CURRENCY} refunded.")
         return False
 
 
-def make_tea(drink_name, order_ingredients):
+def make_tea(drink_name, order_ingredients, qty):
     for item in order_ingredients:
-        resources[item] -= order_ingredients[item]
+        resources[item] -= order_ingredients[item] * qty
     print(f"Here is your {drink_name} ☕️. Enjoy!")
 
 
+def refill(missing_item):
+    if missing_item == "water":
+        resources[missing_item] = 1000
+    elif missing_item == "tea":
+        resources[missing_item] = 2000
+    elif missing_item == "milk":
+        resources[missing_item] = 4000
+    print("Thank you sir, please proceed to machine, its refilled now")
+    return True
+
+
 while is_machine_on:
-    choice = input("Enter  Your Choice  karak , sulemani: ")
+    choice = input("Enter  Your Choice  karak , sulemani: ").lower()
     if choice == "off":
         is_machine_on = False
     elif choice == "report":
@@ -67,12 +78,18 @@ while is_machine_on:
         print(f"Milk: {resources['milk']}ml")
         print(f"Tea: {resources['tea']}g")
         print(f"Money: ${profit}")
-    else:
+    elif choice == "karak" or choice == "sulemani":
+        quantity = int(input(f"How many cups of {choice} would you like to have ? "))
         drink = MENU[choice]
-        if check_resources(drink["ingredients"]):
+        if check_resources(drink["ingredients"], quantity):
             payment = process_coins()
-            if is_payment_done(payment, drink["cost"]):
-                make_tea(choice, drink["ingredients"])
+            if is_payment_done(payment, drink["cost"], quantity):
+                make_tea(choice, drink["ingredients"], quantity)
+        else:
+            missing_ingredient = input("Welcome to counter, what is it that is missing from the machine? "
+                                       "water, milk, tea ?").lower()
+            if refill(missing_ingredient):
+                continue
 
-
-
+    else:
+        print("Hey!!!!!!!!!!!! What are you doing")
